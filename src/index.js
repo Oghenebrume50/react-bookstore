1406efee736ad3b5fd4358b77d38c64992229a2b
Oghenebrume50 <raphlbrume@gmail.com>
2019-10-08 16:57:28 +0100

    bookstore setup with needed dependencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<h1>Good</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Book store</h1>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
