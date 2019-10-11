5ec3824a01b25dbc3efe486dd62ac56da140116a
Oghenebrume50 <raphlbrume@gmail.com>
2019-10-11 06:16:03 +0100

    handlechange now works for both input and select

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -7,16 +7,32 @@ class BooksForm extends React.Component {
       title: '',
       category: ''
     }
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(e){
+    const objName = e.target.name;
+    this.setState({
+      [objName]: e.target.value,
+    });
+  }
+
   render() {
     const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
     return(
       <div>
         <form>
-          <input type='text' placeholder='enter book title' />
-          <select>
+          <input type='text' 
+          placeholder='enter book title'
+          name='title'
+          onChange={this.handleChange}/>
+
+          <select 
+          name='category'
+          value={this.state.category}
+          onChange={this.handleChange}>
             {categories.map((category, idx) => 
-              <option key={idx} value='category'>{category}</option>
+              <option key={idx}>{category}</option>
             )}
           </select>
           <input type='submit' value='submit book'/>
