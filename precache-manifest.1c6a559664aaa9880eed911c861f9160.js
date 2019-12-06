7e0d42e1a48300d9edcb2fe47ee90a11478d82f0
Oghenebrume50 <raphlbrume@gmail.com>
2019-12-06 13:55:47 +0100

    Updates

---
+++ precache-manifest.1c6a559664aaa9880eed911c861f9160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a285b83ec00fcdc0f2901738b30767d",
+    "url": "/react-bookstore/index.html"
+  },
+  {
+    "revision": "6f9a62d52fc8663ef955",
+    "url": "/react-bookstore/static/css/main.9fd69c05.chunk.css"
+  },
+  {
+    "revision": "35199ee8e99939a29d44",
+    "url": "/react-bookstore/static/js/2.a8d05a0d.chunk.js"
+  },
+  {
+    "revision": "6f9a62d52fc8663ef955",
+    "url": "/react-bookstore/static/js/main.a64472a1.chunk.js"
+  },
+  {
+    "revision": "12c9db7f6478d9b647b3",
+    "url": "/react-bookstore/static/js/runtime-main.1b692503.js"
+  }
+]);
